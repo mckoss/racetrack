521b820b99eb57ce394eabfa7f24ce4fffdbfda0
Mike Koss <mike@mckoss.com>
2023-03-09 12:28:56 -0800

    Successfully run puppeteer tests in browser - in WSL

--- src/tests/test-in-browser.ts
+++ src/tests/test-in-browser.ts
@@ -1,35 +1,54 @@
-import {assert} from 'chai';
+import { assert } from 'chai';
+
+import { Stats } from 'mocha';
+
+type TestResults = {
+    stats: Stats,
+    failures: number
+}
 
 import puppeteer, { Browser, Page } from 'puppeteer';
 
 const HOST = 'http://localhost:8080';
 
 suite('Headless browser tests', () => {
-  let browser: Browser;
-  let page:  Page;
+    let browser: Browser;
+    let page: Page;
 
-  setup(async () => {
-    browser = await puppeteer.launch({dumpio: true});
-    page = await browser.newPage();
-    page.setViewport({
-      width: 1080,
-      height: 1080
+    setup(async () => {
+        browser = await puppeteer.launch({ /* dumpio: true */ });
+        page = await browser.newPage();
+        page.setViewport({
+            width: 1080,
+            height: 1080
+        });
+        await page.goto(`${HOST}/test/`);
     });
-    await page.goto(`${HOST}/test/`);
-  });
 
-  teardown(async () => {
-    await browser.close();
-  });
+    teardown(async () => {
+        await browser.close();
+    });
 
-  // test('screenshot', async () => {
-  //   await page.screenshot({ path: 'docs/browser-test.png', fullPage: true });
-  // });
+    // test('screenshot', async () => {
+    //   await page.screenshot({ path: 'docs/browser-test.png', fullPage: true });
+    // });
 
-  test('no initial error shown', async () => {
-    const isHidden = await page.$eval('#error', (elem) => {
-      return window.getComputedStyle(elem).visibility === 'hidden';
-  });
-    assert.isTrue(isHidden);
-  });
+    test('Run browser tests.', async () => {
+        const testResults: TestResults = await page.evaluate(() => {
+            const runner = mocha.run(); // Assumes that the Mocha library is loaded on the page
+            return new Promise(resolve => {
+                runner.on('end', () => {
+                    resolve({
+                        stats: runner.stats,
+                        failures: runner.failures
+                    } as TestResults);
+                });
+            }) as Promise<TestResults>;
+        });
+
+        console.log(`Total tests: ${testResults.stats.tests}`);
+        console.log(`Failures: ${testResults.stats.failures}`);
+        assert.isAtLeast(testResults.stats.tests, 30);
+        assert.equal(testResults.stats.failures, 0);
+    });
 }).timeout(10000);
