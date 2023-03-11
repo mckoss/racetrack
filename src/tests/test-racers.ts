bc69b03db614b85ee9016e57e815cbf0aadbdce2
Mike Koss <mike@mckoss.com>
2023-03-11 05:32:10 -0800

    Test racers.

---
+++ src/tests/test-racers.ts
@@ -0,0 +1,42 @@
+import { assert } from 'chai';
+
+import { Racetrack, OVAL } from '../racetrack.js';
+
+import { update } from '../racers/creeper.js';
+import { MJLRacer1 } from "../racers/mjl-racer1.js";
+
+suite('Racers', function () {
+    if (typeof document === 'undefined') {
+        console.log(`Racers tests can only be run in a browser environment`)
+        return;
+    }
+
+    let canvas: HTMLCanvasElement;
+    let rt: Racetrack;
+
+    // Arrow functions hide the `this` context, so we need to use a regular
+    // function here.
+    setup(function () {
+        canvas = document.createElement('canvas');
+        rt = new Racetrack(canvas, OVAL);
+        const title = document.createElement('h2');
+
+        // Append the racetrack canvas to the page in case we want to look at it.
+        title.textContent = `${this.currentTest!.title}:`;
+        document.body.appendChild(title);
+        document.body.appendChild(canvas);
+    });
+
+    test('Creeper', () => {
+        rt.race(update);
+        rt.run();
+        assert.equal(rt.cars[0].status, 'finished');
+    });
+
+    test('MJL #1', () => {
+        const mjl1 = new MJLRacer1(rt);
+        rt.race(mjl1.update.bind(mjl1));
+        rt.run();
+        assert.equal(rt.cars[0].status, 'finished');
+    });
+});
