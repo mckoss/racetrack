efeeaca04e20009ce92512620bd140b0840fb5c6
Mike Koss <mike@mckoss.com>
2023-03-17 00:47:50 -0700

    Tweak option comparisons.  Still unexpected choices but no crashes.

--- src/racers/speed-racer.ts
+++ src/racers/speed-racer.ts
@@ -1,5 +1,5 @@
 import { CarState, MoveOption } from '../racetrack.js';
-import { Point, add, sub, isZero, scale, sign } from '../points.js';
+import { Point, add, sub, isZero, scale, sign, isEqual } from '../points.js';
 
 import { isSafe } from './racer-helper.js';
 
@@ -14,8 +14,13 @@ function update(state: CarState, options: MoveOption[]): Point {
         return sign(state.velocity);
     }
 
+    let dist: Point | undefined;
+    if (state.crashPosition !== undefined) {
+        dist = sub(state.crashPosition, state.position);
+    }
+
     for (const option of options) {
-        // Not a valid option.
+        // Crashes immediately - not a valid option.
         if (option.distanceToFinish === undefined) {
             continue;
         }
@@ -31,19 +36,64 @@ function update(state: CarState, options: MoveOption[]): Point {
             continue;
         }
 
-        if (state.crashPosition !== undefined) {
-            const dist = sub(state.crashPosition, state.position);
+        if (dist !== undefined) {
             if (!isSafe(v, dist)) {
                 continue;
             }
         }
 
-        if (best === undefined || best.distanceToFinish === undefined ||
-            option.distanceToFinish < best.distanceToFinish) {
+        if (cmpOption(option, best) < 0) {
             best = option;
         }
     }
 
+    if (best === undefined) {
+        console.warn(`No good moves for ${state.position} - max breaking.`);
+    }
     return best?.move || scale(-1, sign(state.velocity));
+
+    function cmpOption(a: MoveOption, b?: MoveOption): number {
+        if (b === undefined) {
+            return -1;
+        }
+
+        let t: number;
+
+        // Prefer move toward the finish.
+        t = testValue(a, b, (x) => x.distanceToFinish!);
+        if (t !== 0) {
+            return t;
+        }
+
+        // Prefer move in same direction as last move.
+        t = testBool(a, b, (x) => isEqual(sign(state.velocity), sign(x.move)));
+        if (t !== 0) {
+            return t;
+        }
+
+        // Prefer move along x,y axes (not diagonals).
+        t = testBool(a, b, (x) => {
+            return x.move[0] === 0 || x.move[1] === 0;
+        })
+        if (t !== 0) {
+            return t;
+        }
+
+        console.log(`${state.position}@${state.velocity}: randomly choosing between ${a.move} and ${b.move}.`);
+        return 0.5 - Math.random();
+    }
 }
 
+// Return negative if only a has boolean attribute, positibe if only b has boolean
+// attribute, 0 if they both have the same boolean attribute.
+function testBool<T>(a: T, b: T, f: (x: T) => boolean): number {
+    const aBool = f(a);
+    const bBool = f(b);
+    return aBool === bBool ? 0 : aBool ? -1 : 1;
+}
+
+function testValue<T>(a: T, b: T, f: (x: T) => number): number {
+    const aVal = f(a);
+    const bVal = f(b);
+    return aVal - bVal;
+}
